d80dbdef99a8d527289ec4a27da35d392d589572
Nathan Orris <nathandavid719@gmail.com>
2020-07-02 02:09:01 -0500

    implemented validation making sure data coming in is actully valid

--- functions/index.js
+++ functions/index.js
@@ -68,6 +68,7 @@ app.post('/signup', (req, res) => {
     }
 
     // TODO: validate data
+    let token, userId;
     db.doc(`/users/${newUser.handle}`).get()
         .then(doc => {
             if(doc.exists){
@@ -79,15 +80,32 @@ app.post('/signup', (req, res) => {
             }
         })
         .then(data => {
+            userId = data.user.uid;
             return data.user.getIdToken();
 
         })
-        .then(token => {
+        .then((idToken) => {
+            token = idToken;
+            // return res.status(201).json({ token });
+            const userCredentials = {
+                handle: newUser.handle,
+                email: newUser.email,
+                createdAt: new Date().toISOString(),
+                userId 
+            }
+            return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+        })
+        .then(() => {
             return res.status(201).json({ token });
         })
+
         .catch(err => {
             console.error(err);
+            if(err.code === 'auth/email-already-in-use'){
+                return res.status(400).json({ email: 'Email is already in use'});
+            } else {
             return res.status(500).json({ error: err.code });
+        }
         })
 
 });
