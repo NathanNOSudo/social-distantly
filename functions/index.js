be325672bab95c08ec2d13aaecda4b32f6f0d1f4
Nathan Orris <nathandavid719@gmail.com>
2020-07-02 01:53:57 -0500

    added logic to check if handle exists or not using json token.

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,29 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
+const app = require('express')();
 admin.initializeApp();
 
-const express = require('express');
-const app = express();
+const config = {
+        apiKey: "AIzaSyDxdLrNCzBSr_h_uE3lvOKf4hz9gZ985Jk",
+        authDomain: "social-distantly.firebaseapp.com",
+        databaseURL: "https://social-distantly.firebaseio.com",
+        projectId: "social-distantly",
+        storageBucket: "social-distantly.appspot.com",
+        messagingSenderId: "170820369123",
+        appId: "1:170820369123:web:e3a1ed46d95a880ff0e3d6",
+        measurementId: "G-S7NT2FEBYV"
+};
+
+const firebase = require('firebase');
+firebase.initializeApp(config);
+
+const db = admin.firestore();
 
 app.get('/screams', (req, res) => {
-    admin
-    .firestore()
+    db
     .collection('screams')
-    .orderBy('createdAt', )
+    .orderBy('createdAt', 'desc' )
     .get()
     .then((data) => {
           let screams = [];
@@ -31,10 +44,10 @@ app.post('/scream', (req, res) => {
     const newScream = {
         body: req.body.body,
         userHandle: req.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()
     };
 
-    admin.firestore()
+    db
     .collection('screams')
     .add(newScream)
     .then((doc) => {
@@ -44,6 +57,39 @@ app.post('/scream', (req, res) => {
         res.status(500).json({error: 'Something is broke my dude'});
         console.error(err);
     });
+});
+// sign up route
+app.post('/signup', (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle,
+    }
+
+    // TODO: validate data
+    db.doc(`/users/${newUser.handle}`).get()
+        .then(doc => {
+            if(doc.exists){
+                return res.status(400).json({ handle: 'this handle is already taken' });
+            } else {
+                return firebase
+            .auth()
+            .createUserWithEmailAndPassword(newUser.email, newUser.password);
+            }
+        })
+        .then(data => {
+            return data.user.getIdToken();
+
+        })
+        .then(token => {
+            return res.status(201).json({ token });
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({ error: err.code });
+        })
+
 });
 
 // https://baseurl.com/api/screams is best practice some cases api will start baseurl.com
